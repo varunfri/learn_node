9f60bf239fb9f451bba6dc8446cd63bc66b8d1a3
varunfri <varunvishwanath1@gmail.com>
2026-02-16 21:13:47 +0530

    added waiting time for home server

--- db_config/mysql_connect.js
+++ db_config/mysql_connect.js
@@ -9,6 +9,8 @@ export const mysql_db = mysql.createPool({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASS,
     database: process.env.MYSQL_DB,
+    connectTimeout: process.env.MYSQL_HOST == "localhost" ? 0 : 15000, // 30 seconds
+    acquireTimeout: process.env.MYSQL_HOST == "localhost" ? 0 : 15000, // 30 seconds
     multipleStatements: true,
     waitForConnections: true,
     dateStrings: true
